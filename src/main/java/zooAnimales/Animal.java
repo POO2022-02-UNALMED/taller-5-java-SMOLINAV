6975a0e98904d445daa9e623bafdf97932febb9c
SMOLINAV <111089184+SMOLINAV@users.noreply.github.com>
2022-09-26 21:05:07 -0500

    :100:

---
+++ src/main/java/zooAnimales/Animal.java
@@ -0,0 +1,97 @@
+package zooAnimales;
+import java.util.ArrayList;
+import gestion.Zona;
+
+public class Animal {
+	private static int totalAnimales;
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	private ArrayList<Zona> zona = new ArrayList<Zona>();
+	
+	public Animal(String nombre, int edad, String habitat, String genero) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.habitat = habitat;
+		this.genero = genero;
+	}
+	public Animal() {}
+	
+	public String movimiento() {
+		return "desplazarse";
+	}
+	public ArrayList<Zona> getZona() {
+		return zona;
+	}
+	public void setZona(ArrayList<Zona> zona) {
+		this.zona = zona;
+	}
+	public static void setTotalAnimales(int totalAnimales) {
+		Animal.totalAnimales = totalAnimales;
+	}
+	public static int getTotalAnimales() {
+		return totalAnimales;
+	}
+	public static void setTotalAnimales() {
+		Animal.totalAnimales++;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getHabitat() {
+		return habitat;
+	}
+	public void setHabitat(String habitat) {
+		this.habitat = habitat;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public void setZona(Zona nZona) {
+		zona.add(nZona);
+	}
+	
+	public static String totalPorTipo() {
+		int nMamiferos = Mamifero.cantidadMamiferos();
+		int nAves = Ave.cantidadAves();
+		int nReptiles = Reptil.cantidadReptiles();
+		int nPeces = Pez.cantidadPeces();
+		int nAnfibios = Anfibio.cantidadAnfibios();
+		return "Mamiferos: " + nMamiferos+
+				"\nAves: " + nAves+
+				"\nReptiles: " + nReptiles+
+				"\nPeces: " + nPeces+
+				"\nAnfibios: " + nAnfibios;
+	}
+	@Override
+	public String toString() {
+		if(zona.size() == 1) {	
+			return "Mi nombre es "+getNombre()+
+					", tengo una edad de "+getEdad()+
+					", habito en "+getHabitat()+
+					" y mi genero es "+getGenero()+
+					", la zona en la que me ubico es "+zona.get(0)+
+					", en el "+zona.get(0).getZoo();
+		}else {
+			return "Mi nombre es "+getNombre()+
+					", tengo una edad de "+getEdad()+
+					", habito en "+getHabitat()+
+					" y mi genero es "+getGenero();
+		}
+	}
+	
+	
+}
---
+++ src/main/java/zooAnimales/Pez.java
@@ -0,0 +1,59 @@
+package zooAnimales;
+import java.util.ArrayList;
+
+public class Pez extends Animal{
+	private static ArrayList<Pez> listado = new ArrayList<Pez>();
+	public static int salmones;
+	public static int bacalaos;
+	private String colorEscamas;
+	private int cantidadAletas;
+	
+	public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
+		super(nombre, edad, habitat, genero);
+		this.colorEscamas = colorEscamas;
+		this.cantidadAletas = cantidadAletas;
+		listado.add(this);
+	}
+	public Pez() {
+		listado.add(this);
+	}
+	public static int cantidadPeces() {
+		return Pez.listado.size();
+	}
+	public static Pez crearSalmon(String nombre, int edad, String genero) {
+		Pez salmon = new Pez(nombre, edad, "oceano", genero, "rojo", 6);
+		Pez.listado.add(salmon);
+		Pez.salmones++;
+		Animal.setTotalAnimales();
+		return salmon;
+	}
+	public static Pez crearBacalao(String nombre, int edad, String genero) {
+		Pez bacalao = new Pez(nombre, edad, "oceano", genero, "gris", 6);
+		Pez.listado.add(bacalao);
+		Pez.bacalaos++;
+		Animal.setTotalAnimales();
+		return bacalao;
+	}
+	@Override
+	public String movimiento() {
+		return "nadar";
+	}
+	public ArrayList<Pez> getListado() {
+		return Pez.listado;
+	}
+	public void setListado(ArrayList<Pez> listado) {
+		Pez.listado = listado;
+	}
+	public String getColorEscamas() {
+		return colorEscamas;
+	}
+	public void setColorEscamas(String colorEscamas) {
+		this.colorEscamas = colorEscamas;
+	}
+	public int getCantidadAletas() {
+		return cantidadAletas;
+	}
+	public void setCantidadAletas(int cantidadAletas) {
+		this.cantidadAletas = cantidadAletas;
+	}
+}
